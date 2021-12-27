e7a48c7a834bdd6a7040a0507fd13664a9541837
Dishank Jindal <dishankjindalitn@gmail.com>
2021-12-27 13:35:21 +0530

    webview final commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,25 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:heaven_riders_india/view_model/app_state_view_modal.dart';
-import 'package:provider/provider.dart';
-import 'modal/router/app_router.gr.dart';
-import './theme.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(create: (_) => AppStateViewModal()),
-      ],
-      builder: (context, child) {
-        return MyApp();
-      },
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
-
-  final AppRouter _appRouter = AppRouter();
-  final Future<FirebaseApp> _init = Firebase.initializeApp();
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _init,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp.router(
-            debugShowCheckedModeBanner: false,
-            title: 'Heaven Riders India',
-            routeInformationParser: _appRouter.defaultRouteParser(),
-            routerDelegate: _appRouter.delegate(),
-            routeInformationProvider: _appRouter.routeInfoProvider(),
-            theme: myAppThemeLight(),
-            darkTheme: myAppThemeDark(),
-            themeMode: ThemeMode.system,
-          );
-        }
-        return const MaterialApp(
-          home: Scaffold(
-            body: Center(child: CircularProgressIndicator()),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: WebView(
+            initialUrl: 'https://www.heavenridersindia.com/',
+            javascriptMode: JavascriptMode.unrestricted,
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
--- lib/modal/utils/app_state.dart
+++
@@ -1,3 +0,0 @@
-enum ViewState { ideal, busy }
-enum AuthState { signIn, signUp }
-enum Status { initial, loading, completed, error }
